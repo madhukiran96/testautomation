163e6f0aa6e088a6285529616e97ada7062dfd6a
madhukiran96 <64535811+madhukiran96@users.noreply.github.com>
2020-04-29 21:29:29 +0530

    Delete t8.js

--- t8.js
+++
@@ -1,8 +0,0 @@
-describe('download works', () => {
-    it('clicks the link "type"', () => {
-      cy.visit('https://nodejs.org/en/')
-  //fail
-      cy.contains('lsmdnjjckc').click()
-      cy.contains('opposkssnwj').click()
-    })
-  })
