ecb75dcc1f04cbeca3ce850bea6a0ba1ce9788e2
madhukiran96 <64535811+madhukiran96@users.noreply.github.com>
2020-04-29 19:03:53 +0530

    Update t7.js

--- t7.js
+++ t7.js
@@ -1,11 +1,8 @@
-describe('nav bar existence with atleast 1 listing', () => {
+describe('find logo', () => {
     it('nav should have home', () => {
       cy.visit('https://nodejs.org/en/')
 
      cy.get('.container').find('#logo').children('img[src="/static/images/logo.svg"]')
-  
-
-
       
     })
   })
