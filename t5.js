ed81f92c453f7cacb02d595f8123b670661dd1e8
madhukiran96 <64535811+madhukiran96@users.noreply.github.com>
2020-04-29 21:28:34 +0530

    Delete t5.js

--- t5.js
+++
@@ -1,9 +0,0 @@
-describe('nav bar existence with atleast 1 listing', () => {
-    it('nav should have home', () => {
-      cy.visit('https://nodejs.org/en/')
-  
-      cy.get('nav')              
-      .should('contain', 'Home')
-      
-    })
-  })
