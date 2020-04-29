310fef7a275b2330ea228bfcbd11b30344ede322
madhukiran96 <64535811+madhukiran96@users.noreply.github.com>
2020-04-29 21:27:32 +0530

    Delete t2.js

--- t2.js
+++
@@ -1,8 +0,0 @@
-describe('download works', () => {
-    it('clicks the link "type"', () => {
-      cy.visit('https://nodejs.org/en/')
-  
-      cy.contains('12.16.3 LTS').click()
-      cy.contains('14.0.0 Current').click()
-    })
-  })
