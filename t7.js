b0761eadb187e0faa99c5b9319f50da671b559b2
madhukiran96 <64535811+madhukiran96@users.noreply.github.com>
2020-04-29 21:29:10 +0530

    Delete t7.js

--- t7.js
+++
@@ -1,8 +0,0 @@
-describe('find logo', () => {
-    it('nav should have home', () => {
-      cy.visit('https://nodejs.org/en/')
-
-     cy.get('.container').find('#logo').children('img[src="/static/images/logo.svg"]')
-      
-    })
-  })
