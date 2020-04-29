1a3d3755b3fd318d8e35e078d1bf4eb3a4dedebc
madhukiran96 <64535811+madhukiran96@users.noreply.github.com>
2020-04-29 21:29:44 +0530

    Delete t9.js

--- t9.js
+++
@@ -1,7 +0,0 @@
-//website does not load
-describe('My First Test', () => {
-    it('Does not do much!', () => {
-      cy.visit('https://nodjs.org/en/')
-      cy.url().should('eq','https://nodejs.org/en/')
-    })
-  })
